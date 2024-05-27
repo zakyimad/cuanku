d70c63acd90a7ddea5d12730853fe96e20479634
zarex1110 <zakyimad@gmail.com>
2024-05-27 15:06:24 +0700

    Perbaikan Login dan Register Page
    Telah dirubah login dan register page dari laravel ke basic

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,94 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('layouts/blankLayout')
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+@section('title', 'Login Pages')
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+@section('page-style')
+<!-- Page -->
+<link rel="stylesheet" href="{{asset('assets/vendor/css/pages/page-auth.css')}}">
+@endsection
+
+@section('content')
+<div class="position-relative">
+  <div class="authentication-wrapper authentication-basic container-p-y">
+    <div class="authentication-inner py-4">
+
+      <!-- Login -->
+      <div class="card p-2">
+        <!-- Logo -->
+        <div class="app-brand justify-content-center mt-5">
+          <a href="{{url('/')}}" class="app-brand-link gap-2">
+            <span class="app-brand-logo demo">@include('_partials.macros',["height"=>20,"withbg"=>'fill: #fff;'])</span>
+            <span class="app-brand-text demo text-heading fw-semibold">{{config('variables.templateName')}}</span>
+          </a>
         </div>
+        <!-- /Logo -->
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <div class="card-body mt-2">
+            <div class="mb-3 mx-3">
+                <div class="row">
+                    @error('email')
+                        {{ $message }}
+                    @enderror
+                </div>
+                <div class="row">
+                    @error('password')
+                        {{ $message }}
+                    @enderror
+                </div>
+            </div>
+            {{-- <x-auth-session-status class="mb-3" :status="session('status')" /> --}}
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            {{-- <h4 class="mb-2">Welcome to {{config('variables.templateName')}}! 👋</h4> --}}
+          {{-- <p class="mb-4">Please sign-in to your account and start the adventure</p> --}}
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+          <form id="formAuthentication" class="mb-3" action="{{ route('login') }}" method="POST">
+            @csrf
+            <div class="form-floating form-floating-outline mb-3">
+              <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="Enter your email" value="{{old('email')}}" autofocus>
+              <label for="email">Email</label>
+            </div>
+            <div class="mb-3">
+              <div class="form-password-toggle">
+                <div class="input-group input-group-merge">
+                  <div class="form-floating form-floating-outline">
+                    <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
+                    <label for="password">Password</label>
+                  </div>
+                  <span class="input-group-text cursor-pointer"><i class="mdi mdi-eye-off-outline"></i></span>
+                </div>
+              </div>
+            </div>
+            <div class="mb-3 d-flex justify-content-between">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="remember" name="remember">
+                <label class="form-check-label" for="remember-me">
+                  Remember Me
+                </label>
+              </div>
+              <a href="{{url('auth/forgot-password-basic')}}" class="float-end mb-1">
+                <span>Forgot Password?</span>
+              </a>
+            </div>
+            <div class="mb-3">
+              <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+            </div>
+          </form>
+
+          <p class="text-center">
+            <span>Belum punya akun?</span>
+            <a href="{{ route('register') }}">
+              <span>Buat Akun</span>
+            </a>
+          </p>
         </div>
+      </div>
+      <!-- /Login -->
+      <img src="{{asset('assets/img/illustrations/tree-3.png')}}" alt="auth-tree" class="authentication-image-object-left d-none d-lg-block">
+      <img src="{{asset('assets/img/illustrations/auth-basic-mask-light.png')}}" class="authentication-image d-none d-lg-block" alt="triangle-bg">
+      <img src="{{asset('assets/img/illustrations/tree.png')}}" alt="auth-tree" class="authentication-image-object-right d-none d-lg-block">
+    </div>
+  </div>
+</div>
+@endsection
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,83 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('layouts/blankLayout')
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+@section('title', 'Register Pages')
+
+@section('page-style')
+<!-- Page -->
+<link rel="stylesheet" href="{{asset('assets/vendor/css/pages/page-auth.css')}}">
+@endsection
+
+
+@section('content')
+<div class="position-relative">
+  <div class="authentication-wrapper authentication-basic container-p-y">
+    <div class="authentication-inner py-4">
+
+      <!-- Register Card -->
+      <div class="card p-2">
+        <!-- Logo -->
+        <div class="app-brand justify-content-center mt-5">
+          <a href="{{url('/')}}" class="app-brand-link gap-2">
+            <span class="app-brand-logo demo">@include('_partials.macros',["height"=>20])</span>
+            <span class="app-brand-text demo text-heading fw-semibold">{{ config('variables.templateName') }}</span>
+          </a>
         </div>
+        <!-- /Logo -->
+        <div class="card-body mt-2">
+          {{-- <h4 class="mb-2">Adventure starts here 🚀</h4> --}}
+          <p class="mb-4">Silahkan isi formulir berikut !</p>
 
-        <!-- Username -->
-        <div class="mt-4">
-            <x-input-label for="username" :value="__('Username')" />
-            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('username')" class="mt-2" />
-        </div>
+          <form id="formAuthentication" class="mb-3" action="{{route('register')}}" method="POST">
+            @csrf
+            <div class="form-floating form-floating-outline mb-3">
+              <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name" autofocus>
+              <label for="name">Nama</label>
+            </div>
+            <div class="form-floating form-floating-outline mb-3">
+              <input type="text" class="form-control" id="username" name="username" placeholder="Enter your username" autofocus>
+              <label for="username">Username</label>
+            </div>
+            <div class="form-floating form-floating-outline mb-3">
+              <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email">
+              <label for="email">Email</label>
+            </div>
+            <div class="mb-3 form-password-toggle">
+              <div class="input-group input-group-merge">
+                <div class="form-floating form-floating-outline">
+                  <input type="password" id="password" class="form-control" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
+                  <label for="password">Password</label>
+                </div>
+                <span class="input-group-text cursor-pointer"><i class="mdi mdi-eye-off-outline"></i></span>
+              </div>
+            </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="email" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+            <div class="mb-3">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms">
+                <label class="form-check-label" for="terms-conditions">
+                  I agree to
+                  <a href="javascript:void(0);">privacy policy & terms</a>
+                </label>
+              </div>
+            </div>
+            <button class="btn btn-primary d-grid w-100">
+              Sign up
+            </button>
+          </form>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+          <p class="text-center">
+            <span>Sudah punya akun?</span>
+            <a href="{{route('login')}}">
+              <span>Sign in</span>
             </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+          </p>
         </div>
-    </form>
-</x-guest-layout>
+      </div>
+      <!-- Register Card -->
+      <img src="{{asset('assets/img/illustrations/tree-3.png')}}" alt="auth-tree" class="authentication-image-object-left d-none d-lg-block">
+      <img src="{{asset('assets/img/illustrations/auth-basic-mask-light.png')}}" class="authentication-image d-none d-lg-block" alt="triangle-bg">
+      <img src="{{asset('assets/img/illustrations/tree.png')}}" alt="auth-tree" class="authentication-image-object-right d-none d-lg-block">
+    </div>
+  </div>
+</div>
+@endsection
